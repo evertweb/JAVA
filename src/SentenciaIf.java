c69d0741c7dab6d3240ad72c56d1a9b1ea3bb3d1
evert cardenas <cardenasever072@gmail.com>
2024-02-26 17:15:09 -0500

    nuevo cambio v1

--- src/SentenciaIf.java
+++ src/SentenciaIf.java
@@ -2,22 +2,26 @@ public class SentenciaIf {
     public static void main(String[] args) {
 
         int envioDeManzanas=1;
-
-        Boolean manzanaverde=true;
-        if (manzanaverde){
-
-              envioDeManzanas++;
-
-
-            System.out.println("la manzana es de color verde" + envioDeManzanas);
+        int n1=12;
+        int nu2=80;
+        int nu3=9;
+        int nu4=50;
 
 
 
-            }
+        if (n1<nu4 && nu3>n1 || n1<nu2){
+            int numero7=n1+nu4;
+            System.out.println("SE CUMPLEN TODAS LAS CONDICIONES " + numero7);
+        }else {
+            int resta=nu3+nu4;
+            System.out.println("NO SE CUMPLEN TODAS LAS CONDICIONES " + resta);
         }
 
 
 
+
+
+        }
 
     }
 
