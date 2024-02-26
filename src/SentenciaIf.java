0148cc459fec704345c9f7ff20ca7d5a4b195f3b
evert cardenas <cardenasever072@gmail.com>
2024-02-26 08:54:01 -0500

    agrege una nueva sentencia

--- src/SentenciaIf.java
+++ src/SentenciaIf.java
@@ -1,3 +1,27 @@
 public class SentenciaIf {
+    public static void main(String[] args) {
 
-}
+        int envioDeManzanas=1;
+
+        Boolean manzanaverde=false;
+        if (manzanaverde){
+
+            envioDeManzanas +=2;
+
+
+            System.out.println("la manzana es de color verde" + envioDeManzanas);
+
+
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+
+
